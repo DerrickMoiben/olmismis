7928e0e3ba5e8b5b0a341e0092b2a2b4a12a2099
Alogarithm_Ke <derrickmoio92@gmail.com>
2024-05-21 09:12:51 +0300

    form for models

--- farmers/forms.py
+++ farmers/forms.py
@@ -1,10 +1,31 @@
 from django import forms
-
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from farmers.models import Farmer
 
-class FarmerForm(UserCreationForm):
+
+
+
+class SignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'password1', 'password2']
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+class FarmerForm(forms.ModelForm):
+    class Meta:
+        model = Farmer
+        fields = ['name', 'phone', 'address', 'id_number', 'berry_weight']
+
+class FarmerWeightForm(forms.Form):
+    Farmer = forms.CharField(label="Farmer's name", max_length=100)
+    berry_weight = forms.FloatField(label="Coffe berries weight (kg)")
+
+    def clean_Farmer(self):
+        farmer_name = self.cleaned_data.get('Farmer')
+        if not Farmer.objects.filter(name=farmer_name).exists():
+            raise forms.ValidationError('Farmer does not exist')
+        return farmer_name
