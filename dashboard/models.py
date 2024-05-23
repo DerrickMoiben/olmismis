0ab6d2b416b1f3dfceb3ff90e08665218b707004
Alogarithm_Ke <derrickmoio92@gmail.com>
2024-05-23 14:23:27 +0300

    css

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,13 +1,6 @@
 from django.db import models
 
 
-#class Farmer(models.Model):
-    ###location = models.TextField()
-   #id_number = models.CharField(max_length=100)
-    #berry_weight = models.FloatField(null=True, blank=True)
-    
-    #def __str__(self):
-       # return self.name
 class Farmer(models.Model):
     id_number = models.CharField(max_length=100, null=True, blank=True)
     name = models.CharField(max_length=100)
